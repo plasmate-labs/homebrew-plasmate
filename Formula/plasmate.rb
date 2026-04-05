bdb7694a1490371162479397e02fc2f1ca60f1af
DB Hurley <dbhurley@me.com>
2026-04-05 17:18:42 -0400

    chore: bump plasmate formula to v0.5.0

--- Formula/plasmate.rb
+++ Formula/plasmate.rb
@@ -1,26 +1,26 @@
 class Plasmate < Formula
   desc "The browser engine for AI agents - HTML in, Semantic Object Model out"
   homepage "https://plasmate.app"
-  version "0.4.0"
+  version "0.5.0"
   license "Apache-2.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/plasmate-labs/plasmate/releases/download/v0.4.0/plasmate-aarch64-macos"
-      sha256 "de569f39e98e67c73b6a09da78621581e812fed6bcae2ddbd2f979d6b1faee05"
+      url "https://github.com/plasmate-labs/plasmate/releases/download/v0.5.0/plasmate-aarch64-macos"
+      sha256 "78c24dcdf24b73815d61ef3d54e94648ba5907b63cab3d4fe7874ea72117211d"
     else
-      url "https://github.com/plasmate-labs/plasmate/releases/download/v0.4.0/plasmate-x86_64-macos"
-      sha256 "4cbe4326daa0247f310d03102cc657bab831cfd15179d9f4a1384ab20cb64b7d"
+      url "https://github.com/plasmate-labs/plasmate/releases/download/v0.5.0/plasmate-x86_64-macos"
+      sha256 "d29b15f1e38f1914c4eeb63b994ed75f8b46de541fd637063fce0e17c9274ce3"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/plasmate-labs/plasmate/releases/download/v0.4.0/plasmate-aarch64-linux"
-      sha256 "7e5cd2cdde69503a32acc099091be9106caf6c5a78508bf64e4ee4c1736fa68e"
+      url "https://github.com/plasmate-labs/plasmate/releases/download/v0.5.0/plasmate-aarch64-linux"
+      sha256 "888ef0103954e09c0c805d6e2669bc12726f4e5043cdac54940a0bf64f461dc1"
     else
-      url "https://github.com/plasmate-labs/plasmate/releases/download/v0.4.0/plasmate-x86_64-linux"
-      sha256 "6052f24840c410811346a4e18c91feba7f8817d767dcbe37c49bad6e6557526a"
+      url "https://github.com/plasmate-labs/plasmate/releases/download/v0.5.0/plasmate-x86_64-linux"
+      sha256 "197a9eeaa277641fe548eb43e50fceee2335a840ba1a3786332d2fa92b1d506b"
     end
   end
 
